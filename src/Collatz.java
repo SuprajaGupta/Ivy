55c2036a78890698fef1ce2e17e1827c5021dc16
SuprajaGupta <Supraja.Eeramalla@ivycomptech.com>
2022-08-08 21:37:40 +0530

    yayy

--- src/Collatz.java
+++ src/Collatz.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public class Collatz {
         public static void main(String args[]) {
-            Scanner SC=new Scanner(System.in);
-            System.out.println("Enter N");
-            int n=SC.nextInt();
-            System.out.println(n+" ");
+            Scanner sc =new Scanner(System.in);
+            System.out.println("Enter Number ");
+            int num=sc.nextInt();
+            System.out.println(num+" ");
             int count=0;
-            while(n!=1)
+            while(num!=1)
             {
-                if(n%2==0)
+                if(num%2==0)
                 {
-                    n=n/2;
+                    num=num/2;
                     count++;
                 }
                 else
                 {
-                    n=3*n+1;
+                    num=3*num+1;
                     count++;
                 }
-                System.out.println(n);
+                System.out.println(num);
             }
             System.out.println(count);
         }
--- src/Dora.java
+++ src/Dora.java
@@ -1,49 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
 public class Dora {
-    public static void main(String args[])
-    {
-        Scanner SC=new Scanner(System.in);
-        System.out.println("Enter no of rows");
-        int n=SC.nextInt();
-        System.out.println("Enter no of columns");
-        int m=SC.nextInt();
-        System.out.println("Enter the tree number");
-        int tno=SC.nextInt();
-        int[][] mat = new int[n][m];
-        int i=0,j=0;
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<m;j++)
-            {
-                mat[i][j]=SC.nextInt();
+    public static void main(String[] args) {
+        int k = 0;
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int number = sc.nextInt();
+        for (int i = 1; i <= row; i++) {
+            if (number == i) {
+                k = 1;
+                break;
             }
         }
-        i=0;
-        for(j=0;j<m;j++)
-        {
-            if(mat[i][j]==tno)
-            {
-                System.out.println("Yes");
+        for (int i = row; i <= col * row; i = i + row) {
+            if (number == i) {
+                k = 1;
+                break;
             }
-
         }
-        j=0;
-        for(i=0;i<n;i++)
-        {
-            if(mat[i][j]==tno)
-            {
-                System.out.println("Yes");
+        for (int i = 1; i <= (row*(col - 1) + 1); i = i + row) {
+            if (number == i) {
+                k = 1;
+                break;
             }
-
         }
-        j=m-1;
-        for(i=0;i<n;i++)
-        {
-            if(mat[i][j]==tno)
-            {
-                System.out.println("Yes");
-            }
-
-        }
+        if (k == 1)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
     }
 }

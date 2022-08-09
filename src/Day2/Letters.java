5b46664987efc129fd901061e7d2d6ff97fc1327
SuprajaGupta <Supraja.Eeramalla@ivycomptech.com>
2022-08-09 22:08:10 +0530

    SwapCase

--- src/Day2/Letters.java
+++ src/Day2/Letters.java
@@ -1,2 +1,47 @@
-package Day2;public class Letters {
-}
+package Day2;
+import java.util.*;
+
+public class Letters {
+       public static StringBuffer swapCase(String s)
+        {
+        StringBuffer str =new StringBuffer(s);
+        for (int i = 0; i < str.length(); i++) {
+        Character c = str.charAt(i);
+        if (Character.isLowerCase(c))
+        str.replace(i, i + 1,Character.toUpperCase(c) + "");
+        else
+        str.replace(i, i + 1,Character.toLowerCase(c) + "");
+        }
+        return str;
+        }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string=sc.nextLine();
+        System.out.println("String after swapping cases is "+(swapCase(string)));
+        }
+
+        }
+
+
+
+//        public static void main(String[] args)
+//        {
+//            Scanner sc =new Scanner(System.in);
+//            System.out.println("Enter a String");
+//            String str= sc.nextLine();
+//            String s = " ";
+//            for(int i=0;i<s.length();i++)
+//            {
+//                if(isLowerCase(str.charAt(i)))
+//                {
+//                    s=s+toUpperCase(str.charAt(i));
+//                }
+//                else {
+//                    s=s+toLowerCase(str.charAt(i));
+//                }
+//            }
+//            System.out.println(s);
+//        }
+//    }
+
+

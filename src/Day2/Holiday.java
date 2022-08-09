f5cb9a2b784b9d44b374d0b064a321ec11a70932
SuprajaGupta <Supraja.Eeramalla@ivycomptech.com>
2022-08-09 17:16:15 +0530

    OOPS example

--- src/Day2/Holiday.java
+++ src/Day2/Holiday.java
@@ -1,2 +1,35 @@
-package Day2;public class Holiday {
+package Day2;
+
+public class Holiday {
+    private String name;
+    private int day;
+    private String month;
+    // your code goes here
+    Holiday(String name, int day, String month){
+        this.name=name;
+        this.day=day;
+        this.month=month;
+    }
+    public static Boolean inSameMonth(Holiday h1,Holiday h2){
+        if(h1.month.equals(h2.month)){
+            return true;
+        }
+        return false;
+    }
+    public Double avgDate(Holiday h[]) {
+        double avg = 0;
+        for (int i = 0; i < h.length; i++) {
+            this.day = h[i].day;
+            avg += this.day;
+        }
+        return (double) avg / h.length;
+    }
+
+    public static void main(String[] args) {
+        Holiday h1=new Holiday("Independence Day",4,"August");
+        Holiday h2=new Holiday("Republic Day",26,"January");
+        System.out.println(Holiday.inSameMonth(h1,h2));
+        System.out.print(h1.name);
+
+    }
 }
--- src/Day2/PolymorphismDemo.java
+++ src/Day2/PolymorphismDemo.java
@@ -1,24 +1,25 @@
 package Day2;
 
-class Parent{
+class Parent extends PolymorphismDemo{
     public static void add(int a,int b){
         //return 10;
         System.out.println("parent");
     }
+    public static void main(String[] args){
+        //System.out.println(add(1,2));
+        add(1,2);
+
+    }
 }
-        public class PolymorphismDemo {
+public class PolymorphismDemo {
 
     public static void add(int number1,int number2){
         System.out.println("Child" +number1+number2);
 
     }
-    public static int add(int number1,int number2){
-        System.out.println(number1+number2);
-        //return(number1+number2);
-    }
+//    public static int add(int number1,int number2){
+//        System.out.println(number1+number2);
+//        //return(number1+number2);
+//    }
 
-public static void main(String[] args){
-    //System.out.println(add(1,2));
-        add(1,2);
-
-}}
+}
